8593b1fc63b549d7c764519fa16b2c8888d90458
marwan <maromaro792009@gmail.com>
2026-09-03 15:58:51 +0300

    Fix typo in Access-Control-Allow-Origin header

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const wss = new WebSocketServer({ noServer: true });
 const PORT = process.env.PORT;
 
 app.use((req, res, next) => {
-    // res.setHeader('Acess-Control-Allow-Origin', 'http://localhost:5500')
-    res.setHeader('Acess-Control-Allow-Origin', 'https://maromaro792009-tech.github.io/ChatMe/public/')
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500')
+    res.setHeader('Access-Control-Allow-Origin', 'https://maromaro792009-tech.github.io/ChatMe/public/')
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
