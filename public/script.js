bbbe44b950231f2464ca7b7e08b92e0aa83a01b4
marwan <maromaro792009@gmail.com>
2026-09-07 16:50:21 +0300

    Add service worker registration
    Register service worker and handle registration success and error.

--- public/script.js
+++ public/script.js
@@ -605,3 +605,12 @@ notifcations.observe(requestsArea, {
     characterData: true
 })
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => {
+      console.log("Service worker registered");
+    })
+    .catch((error) => {
+      console.error("Service worker registration failed:", error);
+    });
+}
